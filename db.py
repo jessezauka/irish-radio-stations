6d3560b9696acbfdf6f019d8bd9a728c9a5cb7e5
Jesse Zauka <51424566+jessezauka@users.noreply.github.com>
2020-06-06 10:44:42 +0100

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-
-CONNECTION_STRING = "mongodb+srv://Ceslovas:Microsoftsux1984@cookbook-jqvqs.mongodb.net/radio-stations?retryWrites=true&w=majority"
-
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('cookbook')
-user_collection = pymongo.collection.Collection(db, 'user_collection')
